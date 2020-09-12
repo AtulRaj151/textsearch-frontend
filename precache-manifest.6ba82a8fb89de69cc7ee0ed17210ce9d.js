c8e81dc5218ee78ed4ce0f92411ca4ec24b26f8d
Atul Raj <atulsid151@gmail.com>
2020-09-13 03:42:34 +0530

    Updates

---
+++ precache-manifest.6ba82a8fb89de69cc7ee0ed17210ce9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498474aaba848a8f41fd2ca8198afd38",
+    "url": "/textsearch-frontend/index.html"
+  },
+  {
+    "revision": "01dde18ed4edcc9c1494",
+    "url": "/textsearch-frontend/static/css/main.0c80568f.chunk.css"
+  },
+  {
+    "revision": "c1fd284108e30e4c8f75",
+    "url": "/textsearch-frontend/static/js/2.31c06e99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/textsearch-frontend/static/js/2.31c06e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01dde18ed4edcc9c1494",
+    "url": "/textsearch-frontend/static/js/main.f3d2b3b9.chunk.js"
+  },
+  {
+    "revision": "f8684aa4719730cd4399",
+    "url": "/textsearch-frontend/static/js/runtime-main.b8579780.js"
+  }
+]);
